57e55e66ec7b8b850f17690c7ee6b4f90ff2e243
marcoshanono35-dot <marcoshanono35@gmail.com>
2025-11-14 23:28:42 -0500

    Update ADuserPassChange.ps1

--- ADuserPassChange.ps1
+++ ADuserPassChange.ps1
@@ -1,18 +1,26 @@
 $acc = Get-ADUser -Filter * | Select-Object -ExpandProperty SamAccountName
+$skip = @("krbtgt", "DefaultAccount", "Guest", "WDAGUtilityAccount") | ForEach-Object { $_.Trim().ToLower() }
 
 $results = @()
 
 foreach ($user in $acc) {
+
+    # Skip system accounts
+    if ($skip -contains $user.ToLower()) {
+        Write-Host "Skipping $user"
+        continue
+    }
+
     Write-Host "`nSet password for: $user"
     $Password = Read-Host "Enter the new password for $user" -AsSecureString
-    
+        
     Set-ADAccountPassword -Identity $user -NewPassword $Password -Reset
 
     $PasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
         [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
     )
 
-    $results+= [PSCustomObject]@{
+    $results += [PSCustomObject]@{
         User     = $user
         Password = $PasswordPlain
     }
