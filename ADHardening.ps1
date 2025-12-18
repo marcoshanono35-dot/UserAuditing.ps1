c194314299cd62ef77fbfbf47ed75df2279c92cc
marcoshanono35-dot <marcoshanono35@gmail.com>
2025-12-18 17:32:09 -0500

    Update ADHardening.ps1

--- ADHardening.ps1
+++ ADHardening.ps1
@@ -260,27 +260,3 @@ foreach ($acc in $allAdmins) {
         Disable-ADAccount -Identity $acc
     }
 }
-
-#password rotation
-$acc = Get-ADUser -Filter * | Select-Object -ExpandProperty SamAccountName
-
-$results = @()
-
-foreach ($user in $acc) {
-    Write-Host "`nSet password for: $user"
-    $Password = Read-Host "Enter the new password for $user" -AsSecureString
-    
-    Set-ADAccountPassword -Identity $user -NewPassword $Password -Reset
-
-    $PasswordPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
-    )
-
-    $results+= [PSCustomObject]@{
-        User     = $user
-        Password = $PasswordPlain
-    }
-}
-
-Write-Host "`n=== PASSWORD SUMMARY ===`n"
-$results | Format-Table -AutoSize
