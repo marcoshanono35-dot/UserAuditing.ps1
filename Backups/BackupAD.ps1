1a7c1e64c3606f21b5fa8d813c2e393916fab059
Marcos Hanono <marcoshanono35@gmail.com>
2025-12-24 20:35:13 -0500

    Update BackupAD.ps1

--- Backups/BackupAD.ps1
+++ Backups/BackupAD.ps1
@@ -1,4 +1,15 @@
-ntdsutil --% "set dsrm password" "reset password on server null" "P@ssword123!" "P@ssword123!" q q
+#ntdsutil --% "set dsrm password" "reset password on server null" "P@ssword123!" "P@ssword123!" q q
+$Password = "P@ssword123!" 
+$DSRM_Script = @(
+    "set dsrm password",
+    "reset password on server null",
+    $Password,
+    $Password,
+    "q",
+    "q"
+)
+
+$DSRM_Script | ntdsutil
 
 $BackupPath = "C:\AD_Backup"
 if (Test-Path $BackupPath) { Remove-Item $BackupPath -Recurse -Force }
