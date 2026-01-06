81b2cc68be03fe5eb9e87d9c565810fffae36586
Marcos Hanono <marcoshanono35@gmail.com>
2026-01-06 18:21:45 -0500

    Update temp.ps1

--- Backups/temp.ps1
+++ Backups/temp.ps1
@@ -1,1 +1,12 @@
-Get-ADObject -Filter * -SearchBase "DC=GREAT,DC=CRETACEOUS" | Group-Object ObjectClass | Select-Object Name, Count
+$s = Get-ADObject -Filter * -SearchBase "DC=GREAT,DC=CRETACEOUS" | Group-Object ObjectClass | Select-Object Count
+
+# Initialize the total variable at 0
+$total = 0
+
+foreach($c in $s){
+    # Add each group's count to the total
+    $total += $c.Count
+}
+
+# Output the final result
+Write-Host "Total Sum of All Records: $total" -ForegroundColor Green
